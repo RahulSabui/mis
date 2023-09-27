e07b528ea2a6030ee53da16810fd274f6c1e99e0
anandsen9237 <anand.sen@techwens.in>
2023-09-27 16:59:11 +0530

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,37 +1,6 @@
 @extends('layouts.app')
 @section('content')
 
-<<<<<<< HEAD
-<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!--favicon-->
-	<link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
-	<!--plugins-->
-	<link href="assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
-	<link href="assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
-	<link href="assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
-	<link href="assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
-	<link href="assets/plugins/datatable/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
-	<!-- loader-->
-	<link href="assets/css/pace.min.css" rel="stylesheet" />
-	<script src="assets/js/pace.min.js"></script>
-	<!-- Bootstrap CSS -->
-	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
-	<link href="assets/css/bootstrap-extended.css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
-	<link href="assets/css/app.css" rel="stylesheet">
-	<link href="assets/css/icons.css" rel="stylesheet">
-	<!-- Theme Style CSS -->
-	<link rel="stylesheet" href="assets/css/dark-theme.css" />
-	<link rel="stylesheet" href="assets/css/semi-dark.css" />
-	<link rel="stylesheet" href="assets/css/header-colors.css" />
-	
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-	<title>Dashboard</title>
-</head>
-=======
 <div class="page-content">
 	<!-- <div class="row mt-4">
 		<div class="col-lg-4">
@@ -39,7 +8,6 @@
 				<div class="card-body">
 					<div class="d-flex align-items-center">
 						<div>
->>>>>>> 438edf82af08d68ad7e712dfae1a541139d4f97d
 
 							<h4 class="my-1">Employee Report</h4>
 							<p class="mb-0 font-13 text-success"><i
